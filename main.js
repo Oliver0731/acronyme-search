6c4b88a76ece7929c833c47d8a1e5a1685dbebc8
Oliver Townell <o.townell@EDU.epsb.ca>
2022-10-26 14:50:28 -0600

    m

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // JavaScript Code
+let inputId = document.getElementById("output");
+let searchId = document.getElementById("search");
+
+document.getElementById("bnt").addEventListener("click", buttonClick);
+
+function buttonClick() {
+  if (searchId === "Nasa") {
+    inputId.innerHTML = "Nastional altermondial super acusition";
+  }
+}
